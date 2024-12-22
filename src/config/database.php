8a3c1d24f79dff4bb02e593667fa92c58c1aa94f
kazu9696 <kazuk6114@gmail.com>
2024-12-22 12:31:34 +0900

    Trigger Heroku deployment to install mbstring

--- src/config/database.php
+++ src/config/database.php
@@ -45,19 +45,19 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => null,
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'strict' => true,
             'engine' => null,
         ],
+
 
         'pgsql' => [
             'driver' => 'pgsql',
