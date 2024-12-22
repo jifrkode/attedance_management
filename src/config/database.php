124f8043140d5b06d461297024458ef797635116
kazu9696 <kazuk6114@gmail.com>
2024-12-22 11:43:20 +0900

    revice mail

--- src/config/database.php
+++ src/config/database.php
@@ -52,15 +52,11 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'charset' => 'utf8', // 修正: utf8mb4 → utf8
+            'collation' => 'utf8_unicode_ci', // 修正: utf8mb4_unicode_ci → utf8_unicode_ci
             'prefix' => '',
-            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
         ],
 
         'pgsql' => [
@@ -90,6 +86,8 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
+
+
 
     ],
 
@@ -123,7 +121,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
