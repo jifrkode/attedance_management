a9f0675141373b5c8e11150b3aebc7579a652676
kazu9696 <kazuk6114@gmail.com>
2024-12-22 12:46:35 +0900

    Add custom php.ini for Heroku

--- src/app/Http/Requests/RegisterRequest.php
+++ src/app/Http/Requests/RegisterRequest.php
@@ -16,8 +16,9 @@ class RegisterRequest extends FormRequest
         return [
             'name' => 'required|string|max:191',
             'email'    => 'required|email|unique:users,email|string|max:191',
-            'password' => 'required|string|min:8|max:191',
+            'password' => 'required|string|min:8|max:191|confirmed',
             'password_confirmation' => 'required',
+            
         ];
     }
 
@@ -37,6 +38,7 @@ class RegisterRequest extends FormRequest
             'password.min'      => 'パスワードは8文字以上でなければなりません。',
             'password.max'      => 'パスワードは最大191文字です。',
             'password_confirmation.required' => '確認用パスワードが不正です。',
+            'password.confirmed' => 'パスワードが一致しません。',
         ];
     }
 }
--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -29,7 +29,7 @@
                 @enderror
             </div>
             <div class="app__form--item">
-                <input type="email" name="password_confirmation" placeholder="確認用メールアドレス">
+                <input type="password" name="password_confirmation" placeholder="確認用パスワード">
                 @error('password_confirmation')
                 <span>{{ $message }}</span>
                 @enderror
