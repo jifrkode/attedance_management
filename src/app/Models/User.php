ef49cb511739944149bda96879b2de19e04fdffc
kazu9696 <kazuk6114@gmail.com>
2025-01-11 19:54:46 +0900

    1

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 
-class User extends Authenticatable implements MustVerifyEmailContract
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -30,9 +30,10 @@ Route::middleware('auth')->group(function () {
 });
 
 // メール送信確認画面
-Route::get('/email-sent', fn() => view('auth.email-sent'))->name('email.sent');
+// Route::get('/email-sent', fn() => view('auth.email-sent'))->name('email.sent');
 // メールテスト
-Route::get('/email-test', [MailTestController::class, 'sendTestEmail'])->name('sendTestEmail');
+Route::get('/email-test', fn() => view('auth.email-sent'))->name('email.sent');
+// Route::get('/email-test', [MailTestController::class, 'sendTestEmail'])->name('sendTestEmail');
 
 // 勤怠管理ルート
 Route::middleware('auth')->prefix('attendance')->name('attendance.')->group(function () {
