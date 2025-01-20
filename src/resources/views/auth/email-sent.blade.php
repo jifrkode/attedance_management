6d261fdff7ffa7cd6e061eebd477d6a2f35a93a4
kazu9696 <kazuk6114@gmail.com>
2025-01-20 17:39:28 +0900

    resend mail

--- src/resources/views/auth/email-sent.blade.php
+++ src/resources/views/auth/email-sent.blade.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>二段階認証メール送信</title>
 </head>
+
 <body>
     <div style="text-align: center; margin-top: 50px;">
         <h1>二段階認証メールを送信しました！</h1>
@@ -12,5 +14,12 @@
         <p>認証が完了したら、以下のリンクからログインしてください。</p>
         <a href="{{ route('login') }}">ログインはこちら</a>
     </div>
+    @if (session('status'))
+    <div class="alert alert-success">
+        {{ session('status') }}
+    </div>
+    @endif
+
 </body>
+
 </html>
